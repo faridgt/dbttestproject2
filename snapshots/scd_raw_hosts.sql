c56e94eac7885c958658b2394b891ae96ecfc409
farid <fmohammad@gmail.com>
2022-11-26 15:12:23 +0200

    scd

---
+++ snapshots/scd_raw_hosts.sql
@@ -0,0 +1,13 @@
+{% snapshot scd_raw_hosts  %}
+
+{{
+   config(
+       target_schema='WORKSPACE_925240422',
+       unique_key='id',
+       strategy='check',
+        check_cols=['name', 'is_superhost'],
+   )
+}}
+select * FROM   {{ source('in.c-airbnb', 'raw-hosts') }} --source use table name mapping from sources.yml
+
+{% endsnapshot %}
